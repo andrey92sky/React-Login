f5cc699d70842e1fa9cee28b19de772410b9f971
= <=>
2020-04-22 18:25:22 -0700

    Add Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Home =()=>{
+const Login =()=>{
+    const [userdata, setUserdata] = useState('');
+
+    const onChange=  (e)=>{
+        setUserdata({
+            ...userdata,
+            [e.target.name]:e.target.value
+        })
+    }
     return (
         <div>
-            This is Home page
+            <form >
+                <label  >
+                    Username:
+                    <input type='text' name='username' value={userdata.username} onChange={(e)=>onChange(e)}></input>
+                </label>
+                <label  >
+                    Password:
+                    <input type='password' name='userpassword' value={userdata.password} onchange={(e)=>onChange(e)}></input>
+                </label>
+                <button type='submit'>Login</button>
+            </form>
         </div>
     )
 }
 
-export default Home;
+export default Login;
